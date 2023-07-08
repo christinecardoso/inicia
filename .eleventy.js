e273b39fcff2030540c92cc27dfb4a800c8bf966
christinecardoso <christinesalafia@gmail.com>
2023-07-07 23:05:25 -0400

    chore: baseUrl

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ const Image = require("@11ty/eleventy-img");
 const yaml = require("js-yaml"); // Because yaml is nicer than json for editors
 require('dotenv').config();
 
-const baseUrl = process.env.BASE_URL || "http://localhost:8080";
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://inicia.netlify.app' : 'http://localhost:8080';
 console.log('baseUrl is set to ...', baseUrl);
 
 const globalSiteData = {
